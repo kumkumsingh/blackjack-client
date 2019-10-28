4c701e18ea5a94150b5228d6dd00a3d3af38372c
kumkumsingh <kumkumsingh.r@gmail.com>
2019-10-28 16:48:53 +0100

    initial home page setup

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Tic-Tac-Toe Game</h1>
+            </div>
+        )
+    }
+}
